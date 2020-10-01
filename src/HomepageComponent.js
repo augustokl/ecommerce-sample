100a6aa8b8aafc7904f4c77947818a98e4ebb15e
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-09-30 23:21:10 -0300

    init ecommerce sample

---
+++ src/HomepageComponent.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import './HomepageComponent.scss';
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <spam className="subtitle">SHOP NOW</spam>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <spam className="subtitle">SHOP NOW</spam>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMAN</h1>
+          <spam className="subtitle">SHOP NOW</spam>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MANS</h1>
+          <spam className="subtitle">SHOP NOW</spam>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">KIDS</h1>
+          <spam className="subtitle">SHOP NOW</spam>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SHOES</h1>
+          <spam className="subtitle">SHOP NOW</spam>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
