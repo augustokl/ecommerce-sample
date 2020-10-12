1a93f23d10a30f189737bfff2dec1b29b1031f80
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-10-12 16:02:21 -0300

    added increase and decrease quantity option on checkout item

--- src/components/CheckoutItem/index.js
+++ src/components/CheckoutItem/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  addItem,
+  clearItemFromCart,
+  removeItem,
+} from '../../redux/cart/cartActions';
 
 import './styles.scss';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+const CheckoutItem = ({ cartItem }) => {
+  const dispatch = useDispatch();
+
+  const { name, imageUrl, price, quantity } = cartItem;
+
+  const onClickRemove = useCallback(() => {
+    dispatch(clearItemFromCart(cartItem));
+  }, [cartItem, dispatch]);
+
+  const onClickArrowIncrese = useCallback(() => {
+    dispatch(addItem(cartItem));
+  }, [cartItem, dispatch]);
+
+  const onClickArrowDecrease = useCallback(() => {
+    dispatch(removeItem(cartItem));
+  }, [cartItem, dispatch]);
+
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={onClickArrowDecrease}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={onClickArrowIncrese}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={onClickRemove}>
+        &#10005;
+      </div>
     </div>
   );
 };
